4a2e7d14067edc570a362b26c46c42fbc0c91245
Gyamerah-ansah <ansahg718@gmail.com>
2025-08-26 10:12:37 -0700

    up

--- JS/fuction.js
+++
@@ -1,5 +0,0 @@
-function Changebackground() {
-    document.body.style.background =
-    document.body.style.background === "lightblue" ? "white" : "lightblue";
-}
-document.getElementById("button").onclick = Changebackground
